081bbc24893b9735dcf353ec98ec107bbd8c72a5
Gabriel <49925197+Darksword333@users.noreply.github.com>
2024-12-02 14:29:59 +0000

    day 1 part 1 fixed

--- day1/part1/part1.py
+++ day1/part1/part1.py
@@ -1,6 +1,7 @@
-with open("/workspaces/aoc2024/day1/part1/input1.txt", "r") as f:
+with open("/workspaces/aoc2024/day1/input.txt", "r") as f:
     input = f.read()
-    input = input.split("\n")
+input = input.split()
+
 
 def main(input):
     list1 = []
@@ -10,21 +11,21 @@ def main(input):
     add = 0
     for i in range(len(input)):
         if i%2 == 0:
-            list1.append(input[i])
+            list1.append(int(input[i]))
         elif i%2 == 1:
-            list2.append(input[i])
-    while len(list1) != 0 and len(list2) != 0:
-        min1 = min(list1)
-        min2 = min(list2)
-        add+= abs(min1 - min2)
-        list1.remove(min1)
-        list2.remove(min2)
+            list2.append(int(input[i]))
+    while len(list1) > 0 and len(list2) > 0:
+        min1 = get_min_index(list1)
+        min2 = get_min_index(list2)
+        add+= abs(list1[min1] - list2[min2])
+        list1.pop(min1)
+        list2.pop(min2)
     return add
 
-def min(liste):
+def get_min_index(liste):
     min = 0
     for i in range(len(liste)):
-        if liste[i] < min:
+        if liste[i] < liste[min]:
             min = i
     return min
 
